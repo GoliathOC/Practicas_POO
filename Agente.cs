949e7f9eb21382f30c4331db603f41d24614e313
GoliathOC <50856499+GoliathOC@users.noreply.github.com>
2019-12-06 23:32:23 -0600

    Entrega Segundo Parcial

---
+++ Agente.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Nombre de desarrollador: Ocampo Coyole Aldo       Materia: Programación Orientada a Objetos    Fecha: 28/11/2019 
+
+public class Agente : MonoBehaviour
+{
+
+    [SerializeField]
+    protected float velocidad;
+    [SerializeField]
+    protected float freno;
+    [SerializeField]
+    protected Transform destinox;
+
+
+    protected void ConfigurarDestino(Transform d)
+    {
+        Vector3 destinoFinal = new Vector3(d.position.x, this.transform.position.y, d.position.z);
+        ConfiguracionFreno(destinoFinal, freno);
+
+        transform.LookAt(destinoFinal);
+
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+    }
+
+    protected void ConfiguracionFreno(Vector3 d, float f)
+    {
+
+        float velocidadLocal = 1;
+        float distancia = Vector3.Distance(transform.position, d);
+
+        if (distancia < f)
+        {
+            velocidad = 0;
+            return (true);
+        }
+
+        else
+        {
+            velocidad = velocidadLocal;
+            return (false);
+        }
+    }
+}
---
+++ CEnemigo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Nombre de desarrollador: Ocampo Coyole Aldo       Materia: Programación Orientada a Objetos    Fecha: 28/11/2019 
+
+public class CEnemigo : MonoBehaviour
+{
+    public string id;
+    public string nombre;
+    public int vida;
+    public int magia;
+
+    EnemigoB enemigoB;
+    Animator animMomia;
+
+    void Start()
+    {
+        enemigoB = FindObjectOfType<EnemigoB>();
+        BusquedaEnemigo(id);
+    }
+    private void BusquedaEnemigo(string id)
+    {
+        for (int i = 0; i < enemigoB.enemigo.Count; i++)
+        {
+            if (id == enemigoB.enemigo[i].nombre)
+            {
+                nombre = enemigoB.enemigo[i].nombre;
+                vida = enemigoB.enemigo[i].vida;
+                magia = enemigoB.enemigo[i].magia;
+            }
+
+        }
+    }
+    private void Update()
+    {
+        ConfigurarDestino(destino);
+
+    }
+}
